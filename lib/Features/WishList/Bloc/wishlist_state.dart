c0214e07ad5b342e131d4b049da3b19de98869ac
System 16 <mittuparmar19@gmail.com>
2023-11-21 18:04:44 +0530

    cart widget added

--- lib/Features/WishList/Bloc/wishlist_state.dart
+++ lib/Features/WishList/Bloc/wishlist_state.dart
@@ -4,7 +4,6 @@ part of 'wishlist_bloc.dart';
 abstract class WishListState {}
 
 class WishListInitialState extends WishListState {}
-class WishListAddedState extends WishListState{}
-class WishListRemovedState extends WishListState{}
-class WishListErrorState extends WishListState{}
+
+
 
